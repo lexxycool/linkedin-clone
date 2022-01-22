efec778904d7a0159f065b79c6f688ed82c5ea2f
Mokwe <Obinna_A_Mokwe@homedepot.com>
2022-01-21 21:38:46 -0600

    header component done

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,14 @@ import React from 'react';
 import "./Header.css"
 import SearchIcon from '@mui/icons-material/Search';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import HeaderOption from './HeaderOption.js';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function Header() {
   return (
@@ -20,7 +28,14 @@ function Header() {
         </div>
 
         <div className="header__right">
+            <HeaderOption Icon={HomeIcon} title="Home"/>
+            <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+            <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+            <HeaderOption Icon={ChatIcon} title="Messaging" />
+            <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+            <HeaderOption Icon={AccountCircleIcon} title="Me" />
 
+            
         </div>
         
         
